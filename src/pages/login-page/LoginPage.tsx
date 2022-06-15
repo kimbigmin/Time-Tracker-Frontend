be24378831397f8c83d4d0d26ad8ca4723263388
kimmingyu <76110448+kimbigmin@users.noreply.github.com>
2022-06-15 22:31:26 +0900

    [Amend]: server url 변경

--- src/pages/login-page/LoginPage.tsx
+++ src/pages/login-page/LoginPage.tsx
@@ -7,7 +7,7 @@ function LoginPage() {
     <Container>
       <Box>
         <img src={logo} alt="logo"></img>
-        <Button href="http://ec2-13-125-194-99.ap-northeast-2.compute.amazonaws.com/api/auth/google">
+        <Button href="hhttp://ec2-52-78-39-53.ap-northeast-2.compute.amazonaws.com/api/auth/google">
           Google 로그인 test?
         </Button>
       </Box>
