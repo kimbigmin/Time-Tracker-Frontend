70077f945faeaa85f30ead3e8b1d92883a57ba49
kimmingyu <76110448+kimbigmin@users.noreply.github.com>
2022-07-16 03:47:44 +0900

    [Design]: 로그인 페이지 반응형 구현

--- src/pages/login-page/style.ts
+++ src/pages/login-page/style.ts
@@ -18,21 +18,33 @@ export const Box = styled.div`
     width: 40%;
     margin-bottom: 2rem;
   }
+
+  @media (max-width: 768px) {
+    width: 70%;
+
+    img {
+      width: 100%;
+    }
+  }
 `;
 
 export const Button = styled.a`
   padding: 1rem;
-  font-size: 1.1rem;
-  font-weight: bold;
-  background-color: #115599;
+  font-size: 1rem;
+  /* font-weight: bold; */
+  background-color: black;
   color: white;
   width: 40%;
-  border-radius: 15px;
+  border-radius: 5px;
   text-align: center;
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
 
   &:hover {
-    background-color: #082a4d;
+    background-color: #282c34;
     transition: 0.5s;
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
   }
 `;
